626dd01e0f090d2ca2c1767e2f0d94070b63a04c
Liebeck <Liebeck@users.noreply.github.com>
2015-04-02 15:24:02 +0200

    Bugfix in ContentEndpoint

---
+++ ZeitOnlineAPISharp/Models/Content/Match.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+
+namespace ZeitOnlineAPISharp.Models.Content
+{
+    public class Match
+    {
+        [JsonProperty("teaser_title")]
+        public String TeaserTitle { get; set; }
+
+        [JsonProperty("uuid")]
+        public String UUID { get; set; }
+
+        [JsonProperty("title")]
+        public String Title { get; set; }
+
+        [JsonProperty("release_date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [JsonProperty("uri")]
+        public String URI { get; set; }
+
+        [JsonProperty("relations")]
+        public List<Relation> Relations { get; set; }
+        
+        [JsonProperty("supertitle")]
+        public String SuperTitle { get; set; }
+
+        [JsonProperty("href")]
+        public String Href { get; set; }
+
+        [JsonProperty("teaser_text")]
+        public String TeaserText { get; set; }
+
+        [JsonProperty("keywords")]
+        public List<Keyword> Keywords { get; set; }
+
+        [JsonProperty("creators")]
+        public List<Creator> Creators { get; set; }
+
+        [JsonProperty("categories")]
+        public List<Category> Categories { get; set; }
+    }
+}
--- ZeitOnlineAPISharp/Models/Content/Response.cs
+++ ZeitOnlineAPISharp/Models/Content/Response.cs
@@ -1,46 +1,15 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ZeitOnlineAPISharp.Models.Content
 {
     public class Response
     {
-        [JsonProperty("teaser_title")]
-        public String TeaserTitle { get; set; }
-
-        [JsonProperty("uuid")]
-        public String UUID { get; set; }
-
-        [JsonProperty("title")]
-        public String Title { get; set; }
-
-        [JsonProperty("release_date")]
-        public DateTime ReleaseDate { get; set; }
-
-        [JsonProperty("uri")]
-        public String URI { get; set; }
-
-        [JsonProperty("relations")]
-        public List<Relation> Relations { get; set; }
-        
-        [JsonProperty("supertitle")]
-        public String SuperTitle { get; set; }
-
-        [JsonProperty("href")]
-        public String Href { get; set; }
-
-        [JsonProperty("teaser_text")]
-        public String TeaserText { get; set; }
-
-        [JsonProperty("keywords")]
-        public List<Keyword> Keywords { get; set; }
-
-        [JsonProperty("creators")]
-        public List<Creator> Creators { get; set; }
-
-        [JsonProperty("categories")]
-        public List<Category> Categories { get; set; }
+        [JsonProperty("matches")]
+        public List<Match> Matches { get; set; }
     }
 }
